51da992a1f261ee4ba79e6f336816973cda2d322
Mayya Yashaswin (MS/EVV4-XC) <myy4kor@bosch.com>
2022-02-09 17:24:29 +0530

    Updated code

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,14 @@
 import range_check
 
 def Tester(BMS_Parameter, BMS_Parameter_value, expected_result):
-
-    if (BMS_Parameter == 'Temperature'):
-      range_check.BMS_Parameter_Range(0, 45)
-    elif (BMS_Parameter == 'SOC'):
-      range_check.BMS_Parameter_Range(20, 80)
-    elif (BMS_Parameter == "Charge Rate"):
-      range_check.BMS_Parameter_Range(0, 0.8)
-
-    assert(range_check.isValueInRange(BMS_Parameter_value) is expected_result)
   
-
-
+  assert(range_check.BMS_Parameter_Range.isValueInRange(BMS_Parameter, BMS_Parameter_value) is expected_result)
+  
 if __name__ == '__main__':
-  Tester('Temperature', 30, True)
+
+  Battery_parameter_Range_Temperature = range_check.BMS_Parameter_Range(0, 45)
+  Battery_parameter_Range_SOC = range_check.BMS_Parameter_Range(20, 80)
+  Battery_parameter_Range_Charge_Rate = range_check.BMS_Parameter_Range(0, 0.8)
+
+  Tester(Battery_parameter_Range_Temperature, 30, True)
   print('All is Well!')
--- range_check.py
+++ range_check.py
@@ -1,9 +1,12 @@
 
 class BMS_Parameter_Range:
 
+    minimum_value = 0
+    maximum_value = 0
+
     def __init__(self,min,max):
         self.minimun_value = min
         self.maximun_value = max
 
     def isValueInRange(self, parameterValue):
-        return not(parameterValue < self.minimum_value or parameterValue > self.minimun_value)
+        return not(parameterValue < self.minimum_value or parameterValue > self.maximum_value)
