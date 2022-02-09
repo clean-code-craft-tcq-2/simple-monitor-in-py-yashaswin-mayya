c7ff5cf975b79f2111331175ef325871b75e4ad5
Mayya Yashaswin (MS/EVV4-XC) <myy4kor@bosch.com>
2022-02-09 17:46:54 +0530

    Updated working code

--- range_check.py
+++ range_check.py
@@ -1,12 +1,9 @@
 
 class BMS_Parameter_Range:
 
-    minimum_value = 0
-    maximum_value = 0
-
     def __init__(self,min,max):
-        self.minimun_value = min
-        self.maximun_value = max
+        self.min = min
+        self.max = max
 
-    def isValueInRange(self, parameterValue):
-        return not(parameterValue < self.minimum_value or parameterValue > self.maximum_value)
+    def isValueInRange(self,parameterValue):
+        return not(parameterValue < self.min or parameterValue > self.max)
