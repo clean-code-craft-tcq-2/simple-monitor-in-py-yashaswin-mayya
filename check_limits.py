9f86b839771be2ecba43c0f4b4cfae412326292d
Mayya Yashaswin (MS/EVV4-XC) <myy4kor@bosch.com>
2022-02-10 15:28:24 +0530

    Updated final working code with snake_text naming convention

--- check_limits.py
+++ check_limits.py
@@ -1,47 +1,47 @@
 import range_check
 
-class BMS_Range_Test:
+class BMS_range_test:
   
-  def Tester(self, BMS_Parameter, BMS_Parameter_value, expected_result):
-    assert(BMS_Parameter.isValueInRange(BMS_Parameter_value) is expected_result)
+  def tester(self, BMS_parameter, BMS_parameter_value, expected_result):
+    assert(BMS_parameter.isValueInRange(BMS_parameter_value) is expected_result)
   
 if __name__ == '__main__':
   
-  Battery_Parameter_Range_for_Temperature = range_check.BMS_Parameter_Range(0, 45)
-  Battery_Parameter_Range_for_SOC = range_check.BMS_Parameter_Range(20, 80)
-  Battery_Parameter_Range_for_Charge_Rate = range_check.BMS_Parameter_Range(0, 0.8)
+  battery_parameter_range_for_temperature = range_check.BMS_parameter_range(0, 45)
+  battery_parameter_range_for_SOC = range_check.BMS_parameter_range(20, 80)
+  battery_parameter_range_for_charge_rate = range_check.BMS_parameter_range(0, 0.8)
 
-  Battery_Tester_Object = BMS_Range_Test()
+  battery_tester_object = BMS_range_test()
 
   #Test Cases for various conditions
   
   #Condition_1 - Values in Valid Range with Equivalence Class Partition(ECP)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Temperature, 22.5, True)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_SOC, 50, True)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Charge_Rate, 0.4, True)
+  battery_tester_object.tester(battery_parameter_range_for_temperature, 22.5, True)
+  battery_tester_object.tester(battery_parameter_range_for_SOC, 50, True)
+  battery_tester_object.tester(battery_parameter_range_for_charge_rate, 0.4, True)
 
   #Condition_2 - Values within range for Boundary Value Analysis(BVA) 
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Temperature, 45-0.1, True)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_SOC, 80-0.1, True)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Charge_Rate, 0.8-0.1, True)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Temperature, 0+0.1, True)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_SOC, 20+0.1, True)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Charge_Rate, 0+0.1, True)
+  battery_tester_object.tester(battery_parameter_range_for_temperature, 45-0.1, True)
+  battery_tester_object.tester(battery_parameter_range_for_SOC, 80-0.1, True)
+  battery_tester_object.tester(battery_parameter_range_for_charge_rate, 0.8-0.1, True)
+  battery_tester_object.tester(battery_parameter_range_for_temperature, 0+0.1, True)
+  battery_tester_object.tester(battery_parameter_range_for_SOC, 20+0.1, True)
+  battery_tester_object.tester(battery_parameter_range_for_charge_rate, 0+0.1, True)
 
   #Condition_3 - Values with bounday value for Boundary Value Analysis(BVA) 
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Temperature, 45, True)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_SOC, 80, True)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Charge_Rate, 0.8, True)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Temperature, 0, True)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_SOC, 20, True)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Charge_Rate, 0, True)
+  battery_tester_object.tester(battery_parameter_range_for_temperature, 45, True)
+  battery_tester_object.tester(battery_parameter_range_for_SOC, 80, True)
+  battery_tester_object.tester(battery_parameter_range_for_charge_rate, 0.8, True)
+  battery_tester_object.tester(battery_parameter_range_for_temperature, 0, True)
+  battery_tester_object.tester(battery_parameter_range_for_SOC, 20, True)
+  battery_tester_object.tester(battery_parameter_range_for_charge_rate, 0, True)
 
   #Condition_4 - Values outside range for Boundary Value Analysis(BVA) 
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Temperature, 45+0.1, False)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_SOC, 80+0.1, False)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Charge_Rate, 0.8+0.1, False)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Temperature, 0-0.1, False)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_SOC, 20-0.1, False)
-  Battery_Tester_Object.Tester(Battery_Parameter_Range_for_Charge_Rate, 0-0.1, False)
+  battery_tester_object.tester(battery_parameter_range_for_temperature, 45+0.1, False)
+  battery_tester_object.tester(battery_parameter_range_for_SOC, 80+0.1, False)
+  battery_tester_object.tester(battery_parameter_range_for_charge_rate, 0.8+0.1, False)
+  battery_tester_object.tester(battery_parameter_range_for_temperature, 0-0.1, False)
+  battery_tester_object.tester(battery_parameter_range_for_SOC, 20-0.1, False)
+  battery_tester_object.tester(battery_parameter_range_for_charge_rate, 0-0.1, False)
 
   print('All is Well!')
--- range_check.py
+++ range_check.py
@@ -1,9 +1,9 @@
 
-class BMS_Parameter_Range:
+class BMS_parameter_range:
 
     def __init__(self, min, max):
         self.minimum_value = min
         self.maximum_value = max
 
     def isValueInRange(self, parameterValue):
-        return not(parameterValue < self.minimum_value or parameterValue > self.maximum_value)
+        return not((parameterValue < self.minimum_value) or (parameterValue > self.maximum_value))
