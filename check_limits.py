bf36a1e386e1c73440e3274da4ae0605e02c4c3b
Mayya Yashaswin (MS/EVV4-XC) <myy4kor@bosch.com>
2022-02-16 11:46:18 +0530

    Updated code to include 2nd extension

--- check_limits.py
+++ check_limits.py
@@ -20,26 +20,10 @@ class BMS_range_test:
     
     battery_parameter_object = parameters_dict.get(BMS_parameter_name)
     self.range_specifier = battery_parameter_object.range_specifier_function(BMS_parameter_value)
-    #self.range_specifier = range_check.BMS_parameter_range(0, 45).range_specifier_function
     self.obtained_result = self.obtained_result_mapping.get(self.range_specifier)
     assert(self.obtained_result is expected_result)
     extensions.print_on_console_main(self.range_specifier, battery_parameter_object, BMS_parameter_name, BMS_parameter_value)
 
-
-
-
-#def print_on_console_main(obtained_result, battery_parameter_object, BMS_parameter_name, BMS_parameter_value):
-    
-  #console_print_string = BMS_parameter_name.replace('_', ' ') #To remove undrscore (_) while displaying on console
-
-  
-  #if (obtained_result == True):
-    #print(f'\n{console_print_string}: {BMS_parameter_value} is within defined range')
-    #print_early_warnings(battery_parameter_object, BMS_parameter_name, BMS_parameter_value)
-  #else:
-    #print(f'\n{console_print_string}: {BMS_parameter_value} is out of defined range')
-
-      
 
   
 if __name__ == '__main__':
--- range_check.py
+++ range_check.py
@@ -6,18 +6,16 @@ class BMS_parameter_range:
         self.maximum_value = max
 
     def range_specifier_function(self, parameterValue):
-        #return not(parameterValue<self.minimum_value or parameterValue>self.maximum_value)
         if(parameterValue<self.minimum_value):
             return 'LOW_BREACH'
         elif(parameterValue>self.maximum_value):
             return 'HIGH_BREACH'
         else:
-            return 'NORMAL'
+            BMS_parameter_range.tolerance_specifier_function(parameterValue)
 
     
     def tolerance_specifier_function(self, parameterValue):
         self.tolerance = 0.05 * self.maximum_value
-        #return (parameterValue<(self.minimum_value+self.tolerance) or parameterValue>(self.maximum_value-self.tolerance))
         if(parameterValue<(self.minimum_value+self.tolerance)):
             return 'LOW_WARNING'
         elif(parameterValue>(self.maximum_value-self.tolerance)):
